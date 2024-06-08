13f09f0d5bcf196f1764986bb43f34838bef280f
Ruivaldo junio <ruivaldo.junior@gmail.com>
2024-06-08 16:03:33 -0300

    five commit

--- script.js
+++ script.js
@@ -1,93 +1,184 @@
 const quizData = [
+
     {
-        question: '1 - Qual a peça do computador é usada fazer a dissipação de calor do processador?',
-        a: 'CPU fan',
-        b: 'GPU fan',
-        c: 'Fonte fan',
-        d: 'HDD fan',
-        e: 'nenhuma da anteriores',
-        correct: 'a'
+        "question": "1 - como é separado a palavra `PEIXE`, sílabicamente?",
+        "a": "PE-I-XE",
+        "b": "PEI-XE",
+        "c": "PEI-X-E",
+        "d": "P-EI-XE",
+        "e": "PEIX-E",
+        "correct": "a"
     },
     {
-        question: '2 - Qual componente é considerado o cérebro do computador?',
-        a: 'Placa mãe',
-        b: 'Memória RAM',
-        c: 'Processador (CPU)',
-        d: 'Disco Rígido (HDD)',
-        e: 'GPU',
-        correct: 'c'
+        "question": "2 - como é separado a palavra `MACACO`, sílabicamente?",
+        "a": "MA-CA-CO",
+        "b": "MA-CAC-O",
+        "c": "MACA-CO",
+        "d": "MA-CA-CO",
+        "e": "MAC-AC-O",
+        "correct": "a"
     },
     {
-        question: '3 - Qual componente é responsável por renderizar gráficos em um computador?',
-        a: 'Placa mãe',
-        b: 'Memória RAM',
-        c: 'Fonte de Alimentação',
-        d: 'Placa de Vídeo (GPU)',
-        e: 'Disco Rígido (HDD)',
-        correct: 'd'
+        "question": "3 - como é separado a palavra `ABACATE`, sílabicamente?",
+        "a": "A-BA-CA-TE",
+        "b": "AB-ACA-TE",
+        "c": "ABAC-A-TE",
+        "d": "AB-ACA-TE",
+        "e": "A-BA-CA-TE",
+        "correct": "a"
     },
     {
-        question: '4 - O que é um SSD?',
-        a: 'Um tipo de processador',
-        b: 'Um tipo de memória de armazenamento',
-        c: 'Um tipo de placa gráfica',
-        d: 'Um tipo de placa mãe',
-        e: 'Um tipo de monitor',
-        correct: 'b'
+        "question": "4 - como é separado a palavra `BORBOLETA`, sílabicamente?",
+        "a": "BOR-BO-LE-TA",
+        "b": "BOR-BOL-ETA",
+        "c": "BORBO-LE-TA",
+        "d": "BOR-BO-LE-TA",
+        "e": "BOR-BO-LETA",
+        "correct": "a"
     },
     {
-        question: '5 - Qual a função da memória RAM em um computador?',
-        a: 'Armazenar dados permanentemente',
-        b: 'Processar instruções do usuário',
-        c: 'Armazenar dados temporariamente para acesso rápido',
-        d: 'Fornecer energia ao sistema',
-        e: 'Conectar todos os componentes',
-        correct: 'c'
+        "question": "5 - como é separado a palavra `CARRO`, sílabicamente?",
+        "a": "CA-R-RO",
+        "b": "CA-RRO",
+        "c": "CAR-RO",
+        "d": "CARR-O",
+        "e": "CARRO",
+        "correct": "c"
     },
     {
-        question: '6 - Qual componente do computador é responsável por fornecer energia elétrica?',
-        a: 'Fonte de Alimentação (PSU)',
-        b: 'Placa mãe',
-        c: 'Processador (CPU)',
-        d: 'Placa de Vídeo (GPU)',
-        e: 'Memória RAM',
-        correct: 'a'
+        "question": "6 - como é separado a palavra `ESPAGUETE`, sílabicamente?",
+        "a": "ES-PA-GUE-TE",
+        "b": "ES-PA-GU-ETE",
+        "c": "ES-PAGU-ETE",
+        "d": "ES-PAG-UE-TE",
+        "e": "ES-PAGUET-E",
+        "correct": "a"
     },
     {
-        question: '7 - Qual dos seguintes componentes é usado para conectar e comunicar todos os outros componentes do computador?',
-        a: 'Placa mãe',
-        b: 'Processador (CPU)',
-        c: 'Memória RAM',
-        d: 'Placa de Vídeo (GPU)',
-        e: 'Disco Rígido (HDD)',
-        correct: 'a'
+        "question": "7 - como é separado a palavra `GIRAFA`, sílabicamente?",
+        "a": "GI-RA-FA",
+        "b": "GIR-A-FA",
+        "c": "GIRAF-A",
+        "d": "GIR-AFA",
+        "e": "GI-RAFA",
+        "correct": "e"
     },
     {
-        question: '8 - Qual é a principal função do disco rígido (HDD) ou SSD?',
-        a: 'Processar dados',
-        b: 'Armazenar dados',
-        c: 'Conectar dispositivos externos',
-        d: 'Fornecer energia',
-        e: 'Refrigerar o sistema',
-        correct: 'b'
+        "question": "8 - como é separado a palavra `PALAVRA`, sílabicamente?",
+        "a": "PA-LA-VRA",
+        "b": "PAL-AV-RA",
+        "c": "PALA-VRA",
+        "d": "PA-LAV-RA",
+        "e": "PALAV-RA",
+        "correct": "a"
     },
     {
-        question: '9 - Qual componente permite a comunicação sem fio em um computador?',
-        a: 'Placa de som',
-        b: 'Placa de rede (NIC)',
-        c: 'Placa Wi-Fi',
-        d: 'Placa de vídeo (GPU)',
-        e: 'Placa mãe',
-        correct: 'c'
+        "question": "9 - como é separado a palavra `COELHO`, sílabicamente?",
+        "a": "CO-E-LHO",
+        "b": "C-OEL-HO",
+        "c": "CO-ELHO",
+        "d": "COEL-HO",
+        "e": "COELHO",
+        "correct": "c"
     },
     {
-        question: '10 - O que significa a sigla "BIOS"?',
-        a: 'Basic Input Output System',
-        b: 'Binary Input Output System',
-        c: 'Basic Internal Output System',
-        d: 'Binary Internal Output System',
-        e: 'Basic Interactive Output System',
-        correct: 'a'
+        "question": "10 - como é separado a palavra `BANANA`, sílabicamente?",
+        "a": "BA-NA-NA",
+        "b": "BA-NAN-A",
+        "c": "BANA-NA",
+        "d": "BAN-ANA",
+        "e": "BANAN-A",
+        "correct": "a"
+    },
+    {
+        "question": "11 - como é separado a palavra `TIGELA`, sílabicamente?",
+        "a": "TI-GE-LA",
+        "b": "TIG-EL-A",
+        "c": "TI-GEL-A",
+        "d": "TI-GELA",
+        "e": "TIGE-LA",
+        "correct": "a"
+    },
+    {
+        "question": "12 - como é separado a palavra `SAPATO`, sílabicamente?",
+        "a": "SA-PA-TO",
+        "b": "SAPA-TO",
+        "c": "SA-PAT-O",
+        "d": "SA-PAT-TO",
+        "e": "SAP-AT-TO",
+        "correct": "a"
+    },
+    {
+        "question": "13 - como é separado a palavra `ÁRVORE`, sílabicamente?",
+        "a": "ÁR-VO-RE",
+        "b": "ÁR-VOR-E",
+        "c": "ÁR-VO-RE",
+        "d": "ÁRV-OR-E",
+        "e": "ÁR-VOR-E",
+        "correct": "a"
+    },
+    {
+        "question": "14 - como é separado a palavra `CONSTRUÇÃO`, sílabicamente?",
+        "a": "CONS-TRU-ÇÃO",
+        "b": "CON-STRU-ÇÃO",
+        "c": "CONS-TRUÇ-ÃO",
+        "d": "CONSTR-U-ÇÃO",
+        "e": "CONSTRU-ÇÃO",
+        "correct": "a"
+    },
+    {
+        "question": "15 - como é separado a palavra `CEREJA`, sílabicamente?",
+        "a": "CE-RE-JA",
+        "b": "CERE-JA",
+        "c": "CE-REJA",
+        "d": "CE-RE-J-A",
+        "e": "CER-E-JA",
+        "correct": "a"
+    },
+    {
+        "question": "16 - como é separado a palavra `AEROPORTO`, sílabicamente?",
+        "a": "AE-RO-POR-TO",
+        "b": "AERO-POR-TO",
+        "c": "AER-OP-OR-TO",
+        "d": "AERO-PORT-O",
+        "e": "AEROP-OR-TO",
+        "correct": "b"
+    },
+    {
+        "question": "17 - como é separado a palavra `DINHEIRO`, sílabicamente?",
+        "a": "DI-NHEI-RO",
+        "b": "DI-NHE-IR-O",
+        "c": "DIN-HEI-RO",
+        "d": "DIN-HE-IR-O",
+        "e": "DIN-H-IR-EO",
+        "correct": "b"
+    },
+    {
+        "question": "18 - como é separado a palavra `ELEFANTE`, sílabicamente?",
+        "a": "E-LE-FAN-TE",
+        "b": "EL-E-FAN-TE",
+        "c": "EL-E-FAN-TE",
+        "d": "E-LEF-AN-TE",
+        "e": "ELE-FAN-TE",
+        "correct": "a"
+    },
+    {
+        "question": "19 - como é separado a palavra `CACHORRO`, sílabicamente?",
+        "a": "CA-CHOR-RO",
+        "b": "CA-CH-OR-RO",
+        "c": "CACH-OR-RO",
+        "d": "CA-CHO-RRO",
+        "e": "CACHO-RRO",
+        "correct": "a"
+    },
+    {
+        "question": "20 - como é separado a palavra `TELEVISÃO`, sílabicamente?",
+        "a": "TE-LE-VI-SÃO",
+        "b": "TEL-E-VI-SÃO",
+        "c": "TE-LE-VIS-ÃO",
+        "d": "TE-LEV-ISÃO",
+        "e": "TELE-VISÃO",
+        "correct": "a"
     }
 ];
 
@@ -109,6 +200,22 @@ function showLaughingEmoji() {
     });
 }
 
+function mostrarNome() {
+    // Pega o valor do input
+    var nome = document.getElementById('nameInput').value;
+
+    // Se o input não estiver vazio
+    if (nome.trim() !== "") {
+        // Esconde o container do input
+        document.getElementById('inputContainer').style.display = 'none';
+
+        // Coloca o valor no h2
+        document.getElementById('displayName').innerText = `Olá ${nome}`;
+    } else {
+        alert("Por favor, digite seu nome.");
+    }
+}
+
 
 const quiz = document.getElementById("quiz");
 const answerEls = document.querySelectorAll(".answer");
@@ -127,11 +234,11 @@ let currentQuiz = 0;
 let score = 0;
 
 loadQuiz();
-function loadQuiz(){
+function loadQuiz() {
     deselectAnswers();
     const currentQuizData = quizData
     [currentQuiz];
-    
+
     questionEl.innerText = currentQuizData.question
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
@@ -143,44 +250,43 @@ function loadQuiz(){
 }
 
 
-function getSelected(){
-  
+function getSelected() {
+
 
     let answer = undefined;
 
     answerEls.forEach((answerEl => {
-        if(answerEl.checked){
+        if (answerEl.checked) {
             answer = answerEl.id;
         }
     }))
     return answer;
 }
 
-function deselectAnswers(){
+function deselectAnswers() {
     answerEls.forEach((answerEl => {
         answerEl.checked = false;
     }))
 }
 
 
-submitBtn.addEventListener('click', () =>{
+submitBtn.addEventListener('click', () => {
     const answer = getSelected();
-        if(answer )
-        {
-            if(answer === quizData[currentQuiz].correct){
-                score++;
-            }
-            currentQuiz++;
-            if(currentQuiz < quizData.length){
-            loadQuiz(); 
-            }else{
-           quiz.innerHTML = `<h2>Você acertou corretamente ${score}/${quizData.length} questões</h2> 
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>Você acertou corretamente ${score}/${quizData.length} questões</h2> 
             <button onclick="location.reload()">Jogar Novamente</button>`
-            if(score < 5){
+            if (score < 5) {
                 showLaughingEmoji();
-            }else{
+            } else {
                 alert("Parabéns meu mano!")
             }
-            }
-       }
+        }
+    }
 });
